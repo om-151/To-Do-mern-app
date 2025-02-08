339fde12173fe38ff3534a77c052f0c23027e8e9
OM SONANI <143808904+om-151@users.noreply.github.com>
2025-02-08 12:27:26 +0530

    Loader added successful

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -112,15 +112,6 @@ export default function Home() {
         }
     };
 
-    // if (loading) {
-    //     return (
-    //         <div className="flex flex-col items-center justify-center min-h-screen">
-    //             <div className="w-16 h-16 border-4 border-dashed rounded-full animate-spin border-blue-500"></div>
-    //             <p className="text-3xl font-bold my-10">Loading...</p>
-    //         </div>
-    //     );
-    // }
-
     if (loading) {
         return (
             <div className="flex flex-col items-center justify-center min-h-screen bg-white">
